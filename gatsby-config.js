a9bcbe93a3b06d97f22fe49bd483f67fc642de56
marlon20m <marlonmora.ndr@gmail.com>
2020-09-01 14:21:26 -0500

    adding package.json outside of folder

---
+++ gatsby-config.js
@@ -0,0 +1,66 @@
+module.exports = {
+  siteMetadata: {
+    title: `Portfolio Marlon Mora`,
+    name: `Marlon Mora`,
+    siteUrl: `https://github.com/marlon20m`,
+    description: `I am a Full Stack Web Developer currently enrolled at The University of Texas`,
+    hero: {
+      heading: `My name is Marlon Mora I am a Full Stack Web Developer`,
+      maxWidth: 652,
+    },
+    social: [
+      {
+        name: `twitter`,
+        url: `https://twitter.com/narative`,
+      },
+      {
+        name: `github`,
+        url: `https://github.com/marlon20m`,
+      },
+      {
+        name: `instagram`,
+        url: `https://instagram.com/narative.co`,
+      },
+      {
+        name: `linkedin`,
+        url: `https://www.linkedin.com/in/giraldo-marlon`,
+      },
+      {
+        name: `dribbble`,
+        url: `https://dribbble.com/narativestudio`,
+      },
+    ],
+  },
+  plugins: [
+    {
+      resolve: "@narative/gatsby-theme-novela",
+      options: {
+        contentPosts: "content/posts",
+        contentAuthors: "content/authors",
+        basePath: "/",
+        sources: {
+          local: true,
+          contentful: false
+        },
+      },
+    },
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `Novela by Narative`,
+        short_name: `Novela`,
+        start_url: `/`,
+        background_color: `#fff`,
+        theme_color: `#fff`,
+        display: `standalone`,
+        icon: `src/assets/letterMFavicon.png`,
+      },
+    },
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: "UA-118232427-3",
+      },
+    },
+  ],
+};
