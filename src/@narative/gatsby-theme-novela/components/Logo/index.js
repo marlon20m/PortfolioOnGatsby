d328637e1f7d51ffd5cf1654f7bb2e10e29fc28a
marlon20m <marlonmora.ndr@gmail.com>
2020-09-03 09:15:39 -0500

    adding updated file with working pictures in posts

---
+++ src/@narative/gatsby-theme-novela/components/Logo/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import logo from "../../../../assets/logomarlon.png"
+
+export default function Logo() {
+    return (
+     <h4>
+        <div className="Image_Small">
+            <img src={logo} alt="Marlon Mora" />
+        </div>
+     </h4>
+    );
+}
